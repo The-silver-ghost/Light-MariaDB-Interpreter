2e434bf7a30d8d6cb3cad9dc8897216b67462c8a
Sanjeevan Rames <thesilverghost2005@gmail.com>
2024-12-18 11:55:28 +0800

    made function to read from file and return contents in a string

---
+++ interpreter/main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <cstdlib> //for os commands
+using namespace std;
+
+ifstream inputFile; //mdb
+ofstream outputFile; // txt
+string inputFilePath,fileContents;
+
+string readFile();
+
+int main() {
+
+		string output = readFile();
+		cout << output  << endl;
+		return 0;
+}
+
+string readFile() {
+	string output;
+
+	cout << "Enter path to input file:" << endl;
+	getline(cin, inputFilePath);
+	inputFile.open(inputFilePath);
+	if (!inputFile) {
+		cout << "File path error." << endl;
+	}
+
+	while (getline (inputFile,fileContents)) {
+		
+		output = output + "\n" + fileContents;
+	}
+	return output;
+}
+
