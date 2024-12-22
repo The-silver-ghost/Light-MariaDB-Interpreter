f8e7953a3d82c8020920236f5d1e20bed94b453d
Sanjeevan Rames <thesilverghost2005@gmail.com>
2024-12-22 13:52:13 +0800

    made program verify file extension of inputfile

--- interpreter/main.cpp
+++ interpreter/main.cpp
@@ -39,24 +39,30 @@ vector<string> readFile() {
 	for (const auto& entry: recursive_directory_iterator(inputFilePath)) {
 	
 		if (is_regular_file(entry.path())) {
-		
-			fileName = entry.path().filename();
-			string newInputFilePath = inputFilePath + fileName;
-			inputFile.open(newInputFilePath);
 
-			while (getline(inputFile,fileContents)) {
+			if (entry.path().extension() == ".mdb"){
+		
+				fileName = entry.path().filename();
+				string newInputFilePath = inputFilePath + fileName;
+				inputFile.open(newInputFilePath);
+
+				while (getline(inputFile,fileContents)) {
 				
-				output.push_back(fileContents);
+					output.push_back(fileContents);
+				}
+				newInputFilePath = inputFilePath;
+				inputFile.close();
 			}
-			newInputFilePath = inputFilePath;
-			inputFile.close();
+			else {
+				cout << "Invalid input file. Perhaps the extension is wrong?" << endl;
+			}
 		}
 
 
 
 		else {
 		
-			cout << "No files detected in directory." << endl;
+			cout << "Object that isn't file detected in directory.\nPlease only keep files in directory" << endl;
 		
 		}
 
