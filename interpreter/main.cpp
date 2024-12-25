ac47fe45de39e5db54a3ecb6cee8655742da66f8
Sanjeevan Rames <thesilverghost2005@gmail.com>
2024-12-25 09:08:35 +0800

    added block of comments for project

--- interpreter/main.cpp
+++ interpreter/main.cpp
@@ -1,3 +1,20 @@
+// *********************************************************
+// Program: YOUR_FILENAME.cpp
+// Course: CCP6114 Programming Fundamentals
+// Lecture Class: TC3L
+// Tutorial Class: TT5L
+// Trimester: 2430
+// Member_1: 243UC245LQ | Sanjeevan A/L Rames | SANJEEVAN.RAMES@student.mmu.edu.my | 0176874937
+// Member_2: 243UC241FF | Youssef M.A. Abdul Razek | youssef.m.a1@student.mmu.edu.my | 966 54 935 7758
+// Member_3: 243UC2400B | Ousama M.A. Abdul Razek | ousama.m.a2@student.mmu.edu.my | +966 50 703 1726
+// Member_4: 243UC246VV | SUHEN KAILASH | suhen.kailash@student.mmu.edu.my | 60 11-1124 0726
+// *********************************************************
+// Task Distribution
+// Member_1:read file, database,update 
+// Member_2:create, select
+// Member_3:table,insert into
+// Member_4:select count,delete
+// *********************************************************
 #include <iostream>
 #include <fstream>
 #include <string>
@@ -36,7 +53,7 @@ vector<string> readFile() {
 	#endif
 	//object from preprocessor
 	recursive_directory_iterator end_itr;
-	for (const auto& entry: recursive_directory_iterator(inputFilePath)) {
+	for (const auto entry: recursive_directory_iterator(inputFilePath)) {
 	
 		if (is_regular_file(entry.path())) {
 
