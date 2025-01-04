eba2ea4099a61b018f580cf5e320878912e4eb2e
YousefAR96 <yousefm_1996@hotmail.com>
2025-01-04 10:32:56 +0300

    Update main.cpp

--- interpreter/main.cpp
+++ interpreter/main.cpp
@@ -23,11 +23,32 @@ using namespace std;
 
 ifstream inputFile;
 string filePath = "C:\\mariadb\\";
-
+struct RowType
+{
+    int userId;
+    string userName;
+    string userCity;
+    string userState;
+    string userCountry;
+    string userPhone;
+    string userEmail;
+};
+void tableDisplay (vector <RowType>& userDetails,ofstream& outfile );
+vector<RowType> intialRows();
 
 vector<string> readFile();
 
 int main() {
+    
+   vector <RowType> userDetails = intialRows();
+
+   ofstream outfile;
+
+   outfile.open("output1.txt");
+
+   tableDisplay (userDetails,outfile );
+
+   outfile.close();
     vector<string> query = readFile();
     for (string commands:query) {
         cout << commands << endl;
@@ -57,4 +78,34 @@ vector <string> readFile() {
         inputFile.close();
     }
     return output;
+}
+vector<RowType> intialRows()
+{
+   return
+   {
+    {1,"name1","city1","state1","country1","phone1","email1"},
+    {2,"name2","city2","state2","country2","phone2","email2"},
+    {3,"name3","city3","state3","country3","phone3","email3"}
+
+};
+
+}
+
+
+void tableDisplay (vector <RowType>& userDetails,ofstream& outfile )
+
+{
+    for (int i = 0; i < userDetails.size() ; i++)
+{
+
+ outfile << userDetails[i].userId<<","
+         << userDetails[i].userName<<","
+         << userDetails[i].userCity<<","
+         << userDetails[i].userState<<","
+         << userDetails[i].userCountry<<","
+         << userDetails[i].userPhone<<","
+         << userDetails[i].userEmail<<endl;
+
+}
+
 }
