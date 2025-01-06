44bfbfd9efaf20899a034801eb3b2461207be207
YousefAR96 <yousefm_1996@hotmail.com>
2025-01-06 14:43:23 +0300

    Update main.cpp

--- interpreter/main.cpp
+++ interpreter/main.cpp
@@ -15,6 +15,24 @@
 // Member_3:table,insert into
 // Member_4:select count,delete
 // *********************************************************
+
+// *********************************************************
+// Program: YOUR_FILENAME.cpp
+// Course: CCP6114 Programming Fundamentals
+// Lecture Class: TC3L
+// Tutorial Class: TT5L
+// Trimester: 2430
+// Member_1: 243UC245LQ | Sanjeevan A/L Rames | SANJEEVAN.RAMES@student.mmu.edu.my | 0176874937
+// Member_2: 243UC241FF | Youssef M.A. Abdul Razek | youssef.m.a1@student.mmu.edu.my | 966 54 935 7758
+// Member_3: 243UC2400B | Ousama M.A. Abdul Razek | ousama.m.a2@student.mmu.edu.my | +966 50 703 1726
+// Member_4: 243UC246VV | SUHEN KAILASH | suhen.kailash@student.mmu.edu.my | 60 11-1124 0726
+// *********************************************************
+// Task Distribution
+// Member_1:read file, database,update
+// Member_2:create, select
+// Member_3:table,insert into
+// Member_4:select count,delete
+// *********************************************************
 #include <iostream>
 #include <fstream>
 #include <string>
@@ -23,37 +41,31 @@ using namespace std;
 
 ifstream inputFile;
 string filePath = "C:\\mariadb\\";
-struct RowType
-{
-    int userId;
-    string userName;
-    string userCity;
-    string userState;
-    string userCountry;
-    string userPhone;
-    string userEmail;
-};
-void tableDisplay (vector <RowType>& userDetails,ofstream& outfile );
-vector<RowType> intialRows();
+
+
+string OutPutFileToUse(vector<string>& query);
+void commandCreateOutPutFile(vector<string>& query, ofstream& outfile);
+void commandCreateTable (vector<string>& query,ofstream& outfile);
 
 vector<string> readFile();
 
 int main() {
-    
-   vector <RowType> userDetails = intialRows();
 
-   ofstream outfile;
-
-   outfile.open("output1.txt");
-
-   tableDisplay (userDetails,outfile );
-
-   outfile.close();
     vector<string> query = readFile();
     for (string commands:query) {
         cout << commands << endl;
     }
-    return 0;
+
+   string outPutName = OutPutFileToUse(query);
+   ofstream outfile;
+   outfile.open(outPutName, ios::app);
+
+   commandCreateOutPutFile(query, outfile);
+   commandCreateTable (query, outfile);
+
+
+   outfile.close();
+
 }
 
 vector <string> readFile() {
@@ -79,33 +91,89 @@ vector <string> readFile() {
     }
     return output;
 }
-vector<RowType> intialRows()
+
+
+string OutPutFileToUse(vector<string>& query)
 {
-   return
+
+    for(int i = 0; i < query.size();i++)
+ {
+     if (query[i].find ("CREATE fileOutput1.txt;")!= -1)
+     {
+
+      return "fileOutput1.txt";
+     }
+    else if (query[i].find ("CREATE fileOutput2.txt;")!= -1)
+     {
+      return "fileOutput2.txt";
+
+     }
+    else if (query[i].find ("CREATE fileOutput3.txt;")!= -1)
+     {
+      return "fileOutput3.txt";
+     }
+
+ }
+  return "";
+}
+
+void commandCreateOutPutFile(vector<string>& query, ofstream& outfile)
+{
+
+    for(int i = 0; i < query.size();i++)
+ {
+     if (query[i].find ("CREATE fileOutput1.txt;")!= -1)
+     {
+      cout << query[i]<<endl;
+      outfile << query[i]<<endl;
+     }
+    else if (query[i].find ("CREATE fileOutput2.txt;")!= -1)
+     {
+      cout << query[i]<<endl;
+      outfile << query[i]<<endl;
+
+     }
+    else if (query[i].find ("CREATE fileOutput3.txt;")!= -1)
+     {
+      cout << query[i]<<endl;
+      outfile << query[i]<<endl;
+     }
+
+ }
+
+}
+
+
+void commandCreateTable(vector<string>& query, ofstream& outfile)
+{
+
+    for(int i = 0; i < query.size();i++)
    {
-    {1,"name1","city1","state1","country1","phone1","email1"},
-    {2,"name2","city2","state2","country2","phone2","email2"},
-    {3,"name3","city3","state3","country3","phone3","email3"}
+        if (query[i].find ("CREATE TABLE")!= -1)
+    {
+        cout <<query[i]<<endl;
+        outfile<<query[i]<<endl;
 
-};
 
+        while (i < query.size() && query[i].find(");") ==-1)
+        {
+            i++;
+            cout<<query[i]<<endl;
+            outfile<<query[i]<<endl;
+        }
+
+        break;
+    }
+
+   }
 }
 
 
-void tableDisplay (vector <RowType>& userDetails,ofstream& outfile )
 
-{
-    for (int i = 0; i < userDetails.size() ; i++)
-{
 
- outfile << userDetails[i].userId<<","
-         << userDetails[i].userName<<","
-         << userDetails[i].userCity<<","
-         << userDetails[i].userState<<","
-         << userDetails[i].userCountry<<","
-         << userDetails[i].userPhone<<","
-         << userDetails[i].userEmail<<endl;
 
-}
 
-}
+
+
+
+
