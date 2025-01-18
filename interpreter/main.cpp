d6adf2fe91760dbfae33136c3bbe79a340ced45d
Sanjeevan Rames <thesilverghost2005@gmail.com>
2025-01-18 16:53:33 +0800

    made update function

--- interpreter/main.cpp
+++ interpreter/main.cpp
@@ -39,6 +39,7 @@ string removeWhitespace(string);
 vector<vector<string>> appendToVector(vector<vector<string>> table,string strToBeAppended);
 void displayCommands(string,string);
 void displayDatabase(int,string);
+vector<vector<string>> updateTable(string,vector<vector<string>>);
 
 int main() {
     string tableName;
@@ -72,7 +73,7 @@ int main() {
             tableDisplay(outfile,customerTable);
         }
         else if (commands.find("UPDATE")!= string::npos) {
-            cout << "UPDATE" << endl;
+            customerTable = updateTable(commands,customerTable);
         }
         else if (commands.find("TABLES")!= string::npos) {
             displayCommands(commands,"TABLES");
@@ -257,4 +258,38 @@ void displayDatabase(int fileNum,string cmd){
         cout << "C:\\mariadb\\fileInput3.mdb" << endl;
         outfile << "C:\\mariadb\\fileInput3.mdb" << endl;
     }
+}
+
+vector<vector<string>> updateTable(string cmd,vector<vector<string>> table){
+    string itemToReplace = cmd;
+    string row = cmd;
+    string columnName = cmd;
+    int colNum;
+    int rowNum;
+
+    displayCommands(cmd,"UPDATE");
+
+    //get item to replace
+    itemToReplace.erase(0,itemToReplace.find("'")+1);
+    itemToReplace.erase(itemToReplace.find("'"),-1);
+
+    //get row
+    row.erase(0,row.find("=")+1);
+    row.erase(0,row.find("=")+1);
+    rowNum = stoi(row);
+
+    //get column name
+    columnName.erase(0,columnName.find("SET")+4);
+    columnName.erase(columnName.find("="),-1);
+
+    for (int vectorRow = 0; vectorRow<table.size();vectorRow++){
+        for (int col = 0; col < table[vectorRow].size();col++){
+            if (table[vectorRow][col] == columnName){
+                colNum = col;
+                break;
+            }
+        }
+    }
+    table[rowNum][colNum] = itemToReplace;
+    return table;
 }
